5dd3a44ad957d951688dbd889df099bfb0ed7f3a
y1683075866 <97658609+y1683075866@users.noreply.github.com>
2022-09-02 20:19:24 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ export const config = {
     APP_SECRET: "0fa61d1a570c9d231b3360f9a33b2172",
 
     // 模板消息id
-    TEMPLATE_ID: "ZvZA8Q5earcA27-5yHzYu_IH9sSv56HnlKeEL1cuXO8",
+    TEMPLATE_ID: "IpRJJRPC-OWGx0l-TAC5VhFyH_1A4sRqsC3qQZLfFf8",
 
     // 回调消息模板id, 用来看自己有没有发送成功的那个模板
     CALLBACK_TEMPLATE_ID: "oxR6O5zl4OSLYIXMAy4TWxy1-wjs",
